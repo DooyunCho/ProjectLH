14f3a1eee8c030bfa538c9453dc4aa50f8184c77
dooyun cho <dooyun.cho@gmail.com>
2020-03-17 06:21:22 +0900

    player move, camera following - camera rotate issue

---
+++ PeojectLH/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraController : MonoBehaviour {
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    public bool LookAtPlayer = false;
+
+    public bool RotateAroundPlayer = true;
+
+    public float RotationsSpeed = 5.0f;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // FixedUpdate is called after Update methods
+    void LateUpdate()
+    {
+        Quaternion camTurnAngleRightandLeft = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationsSpeed, Vector3.up);
+        Quaternion camTurnAngleUpandDown = Quaternion.AngleAxis(-Input.GetAxis("Mouse Y") * RotationsSpeed, Vector3.right);
+
+        _cameraOffset = camTurnAngleUpandDown * camTurnAngleRightandLeft * _cameraOffset;
+
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        transform.LookAt(PlayerTransform);
+
+        PlayerTransform.forward = new Vector3(transform.forward.x, 0, transform.forward.z);
+    }
+}
---
+++ PeojectLH/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+    public float speed = 10.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (characterController.isGrounded)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            moveDirection = (transform.forward * vertical) + (transform.right * horizontal);
+            Debug.Log(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
